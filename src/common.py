0f21fa079a4155b60cf3baa2630ed9eed57de42b
saijithendr <jithenkumar1@gmail.com>
2022-12-03 00:28:46 +0100

    updated scripts

--- src/common.py
+++ src/common.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
+import math
+from sklearn.preprocessing import MinMaxScaler
 
 class TimeSeriesData:
     
@@ -40,4 +42,36 @@ class Config:
     def __init__(self) -> None:
         self.data_path = os.path.join('data', 'window', 'turbine_data.csv')
         self.batch_size = 32
-        self.epochs = 10
+        self.epochs = 1
+
+def split_data(train_data, sequence_len):
+    
+    '''splits the train data into x_train and y_train '''
+    
+    # Splitting the data into x_train, y_train 
+    x_train = []
+    y_train = []
+
+    for i in range(sequence_len, len(train_data)):
+        x_train.append(train_data[i-sequence_len:i , 0])
+        y_train.append(train_data[i, 0])
+        
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    
+    return x_train, y_train
+
+
+def test_data( scaled_data, train_data_len, sequence_len ):
+    
+    test_data = scaled_data[train_data_len-sequence_len: , : ]
+    
+    # create x_test
+    x_test = []
+
+    for i in range(sequence_len , len(test_data)):
+        
+        x_test.append(test_data[i-sequence_len:i, 0])
+        
+    x_test = np.array(x_test)
+    
+    return x_test
